97f138c220839ceff9ef280b9544b70b408a58d1
Mickael <mlouzet@afrique-telecom.com>
2019-07-26 23:48:42 +0200

    (phpstan) : ajout et config de phpstan

---
+++ bin/phpstan-object-manager.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Kernel;
+
+require \dirname(__DIR__).'/config/bootstrap.php';
+
+$kernel = new Kernel($_SERVER['APP_ENV'] ?? 'dev', (bool) $_SERVER['APP_DEBUG']);
+
+$kernel->boot();
+
+return $kernel->getContainer()->get('doctrine')->getManager();
